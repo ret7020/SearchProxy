3a641753f4fd7f5b19a5f2ca4283f78153f9d136
ret7020 <ret7020@gmail.com>
2023-01-07 13:27:39 +0300

    Base stupid version

---
+++ GrayIP/README.md
@@ -0,0 +1,3 @@
+# GrayIP instances
+Development of this module freezed for some time </br>
+Working on WhiteIP instances to create stable base ecosystem
--- VPS/api.py
+++ VPS/api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import config as cfg
 import uvicorn
+import proxy
 
 
 app = FastAPI()
@@ -11,9 +12,10 @@ async def root():
 
 
 @app.get("/search")
-async def search():
-    pass
+async def search(query: str):
+    html = proxy.make_req_requests(query, "EN", res_number=40) 
+    return {"status": 1, "data": html}
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", port=3232, host="0.0.0.0", reload=True) # run async web server
+    uvicorn.run("api:app", port=7777, host="0.0.0.0", reload=True) # run async web server
